8b76b8a45fa21ab90936a2d8b70bd6242c031d09
ParkYunHo <qkrdbsgh0921@gmail.com>
2022-11-28 22:04:54 +0900

    init

---
+++ src/main/kotlin/com/ddd/morningbear/MorningbearApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ddd.morningbear
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MorningbearApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MorningbearApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/ddd/morningbear/MorningbearApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ddd.morningbear
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MorningbearApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
