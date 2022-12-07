8b698e5923c36c7e66db4674f3273eb3ccaa5f0d
ParkYunHo <qkrdbsgh0921@gmail.com>
2022-12-07 22:34:41 +0900

    [login] 토큰정보 인코딩 로직구현

--- src/main/kotlin/com/ddd/morningbear/api/auth/AuthController.kt
+++ src/main/kotlin/com/ddd/morningbear/api/auth/AuthController.kt
@@ -4,6 +4,8 @@ import com.ddd.morningbear.api.auth.dto.AuthResult
 import com.ddd.morningbear.common.annotation.SkipTokenCheck
 import com.ddd.morningbear.common.constants.CommCode
 import com.ddd.morningbear.common.utils.AppPropsUtils
+import com.ddd.morningbear.common.utils.ParseUtils
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -36,7 +38,42 @@ class AuthController {
                 jsKey = AppPropsUtils.findJsKeyByType(CommCode.Social.NAVER.code)!!,
                 nativeKey = AppPropsUtils.findNativeKeyByType(CommCode.Social.NAVER.code)!!,
                 state = CommCode.Social.NAVER.code
+            ),
+            AuthResult(
+                redirectUri = "/login/token",
+                jsKey = AppPropsUtils.findJsKeyByType(CommCode.Social.APPLE.code)!!,
+                nativeKey = AppPropsUtils.findNativeKeyByType(CommCode.Social.APPLE.code)!!,
+                state = CommCode.Social.APPLE.code
             )
         )
+    }
+
+    /**
+     * 토큰정보 인코딩
+     *
+     * @param state [String]
+     * @param token [String]
+     * @return result [String]
+     * @author yoonho
+     * @since 2022.12.07
+     */
+    @SkipTokenCheck
+    @QueryMapping
+    fun encode(@Argument state: String, @Argument token: String): String {
+        return ParseUtils.encode(state, token)
+    }
+
+    /**
+     * 토큰정보 디코딩 - 개발자용
+     *
+     * @param token [String]
+     * @return result [String]
+     * @author yoonho
+     * @since 2022.12.07
+     */
+    @SkipTokenCheck
+    @QueryMapping
+    fun decode(@Argument token: String): String {
+        return ParseUtils.decode(token)
     }
 }
